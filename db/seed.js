ff1c3af56f01de50c5f16d3595dabf3c60fc365d
thefutureseer <danielharrislimos@outlook.com>
2023-04-29 10:24:00 -0700

    begin seed data

--- db/seed.js
+++ db/seed.js
@@ -1,17 +1,5 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config();
-const dbName = process.env.database;
+const sequelize = require('../config/sequelize');
 
-mysql.createConnection({
-    host: "127.0.0.1",
-    port: "3306",
-    user: process.env.user,
-    password : process.env.password
-}).then( connection => {
-    connection.query(`CREATE DATABASE IF NOT EXISTS ${dbName};`).then((res) => {
-        console.info("Database create or successfully checked");
-        process.exit(0);
-    })
-});
+
 
 module.exports = mysql;
